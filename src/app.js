b39ac34bb1da939ce1f875a0ab61f206836de11e
Xiao Xin <xinx1031@gmail.com>
2019-09-07 02:44:38 -0700

    [app] complete api routing.

--- src/app.js
+++ src/app.js
@@ -5,15 +5,39 @@
  * Date: 2019-09-06
  */
 
-// const company = require('company');
-// const investment = require('investment');
-// const news = require('news');
+const company = require('company');
+const investment = require('investment');
+const news = require('news');
 
-// TODO(xinixao): Implement service stub logic.
-const api = {
+const CONFIG = {};
+
+const API = {
   createServiceInstance: (type) => {
-    return null;
+    if (!CONFIG.AUTH) {
+      return undefined;
+    }
+
+    let client;
+    switch (type) {
+      case 'company':
+        client = new company.CompanyApi();
+      case 'investment':
+        client = new investment.InvestmentApi();
+      case 'news':
+        client = new news.NewsApi();
+      default:
+        return undefined;
+    }
+
+    client.apiClient.authentications = CONFIG.AUTH;
+    return client;
+  },
+
+  config: {
+    setAuth: (auth) => {
+      CONFIG.AUTH = auth;
+    },
   },
 };
 
-module.exports = api;
+module.exports = API;
